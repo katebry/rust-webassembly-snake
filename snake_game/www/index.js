6fc779ea0f8f388ac184df00d22e67e624fa9480
katebry <katebryan31@gmail.com>
2023-03-10 14:31:34 +0000

    file clean up

--- snake_game/www/index.js
+++ snake_game/www/index.js
@@ -1,7 +1,21 @@
 async function init() {
+
+    const memory = new WebAssembly.Memory({initial: 1});
+
+    const importObject = {
+        js: {
+          mem: memory
+        },
+        console: {
+            log: () => {
+                console.log("Logging something!!!!");
+            }
+        }
+    }
+
     const response = await fetch("sum.wasm");
     const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer);
+    const wasm = await WebAssembly.instantiate(buffer, importObject);
 
     const sumFunction = wasm.instance.exports.sum;
     const result = sumFunction(100, 80);
