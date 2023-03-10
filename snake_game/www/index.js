dcefce34b74035a25ea3c26fa5c497878a809fec
katebry <katebryan31@gmail.com>
2023-03-10 11:24:28 +0000

    wasm setup

---
+++ snake_game/www/index.js
@@ -0,0 +1,11 @@
+async function init() {
+    const response = await fetch("sum.wasm");
+    const buffer = await response.arrayBuffer();
+    const wasm = await WebAssembly.instantiate(buffer);
+
+    const sumFunction = wasm.instance.exports.sum;
+    const result = sumFunction(100, 80);
+    console.log(result);
+}
+
+init();
