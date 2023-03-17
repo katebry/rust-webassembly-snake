8a790e3a304b97df0bfd3b22529a1a9d3db1d251
katebry <katebryan31@gmail.com>
2023-03-17 16:24:53 +0000

    added express server, rebuilt assets and updated README

---
+++ snake_game/server/index.js
@@ -0,0 +1,19 @@
+const compression = require("compression");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicFolder = path.join(__dirname, "..", "www", "public");
+
+app.use(compression());
+app.use(express.static(publicFolder));
+
+app.get("*", (_, res) => {
+    res.sendFile("index.html")
+})
+
+app.listen(port, () => {
+    console.log("listening...")
+})
